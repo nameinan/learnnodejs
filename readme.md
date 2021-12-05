7646be01433d5adc846f69b9916922e9cf433623
Nandakumar Nameirakpam <nandakumar.nameirakpam@tietoevry.com>
2021-12-05 17:55:18 +0530

    add type orm and sqlexpress connection

--- readme.md
+++ readme.md
@@ -9,3 +9,21 @@ npm run start/npm start
 6. npm i express cors
 7. npm i -D @types/express @types/cors
 8. npm i express-validation --save
+9 Install the npm package type orm:
+ https://typeorm.io/#/
+
+  npm install typeorm --save
+  
+  You need to install reflect-metadata shim:
+  npm install reflect-metadata --save
+
+  and import it somewhere in the global place of your app (for example in app.ts):
+   import "reflect-metadata";
+
+   You may need to install node typings:
+   npm install @types/node --save-dev
+ 
+ Install a database driver:
+ 
+    for Microsoft SQL Server
+    npm install mssql --save
--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,36 @@
 import express ,{Request,Response} from 'express';
 import cors from 'cors'
 import { routes } from './routes';
-
-const app = express();
-app.use(express.json());
-
-
-app.get('/',(req:Request,res:Response)=>{
-    res.send('I am at Amanda');
-});
-
-app.use(cors({
-    origin:["http://localhost:3000"]
-}));
+import "reflect-metadata";
+import { createConnection } from "typeorm";
 
 
-routes(app);
 
 
-app.listen(8000),() => {
-    console.log('listening to port 8000');
-}
+
+
+
+createConnection()
+ .then(() => {
+
+   const app = express();
+   app.use(express.json());
+
+    app.get('/',(req:Request,res:Response)=>{
+        res.send('I am at Amanda');
+    });
+    
+    app.use(cors({
+        origin:["http://localhost:3000"]
+    }));
+    
+    
+    routes(app);
+    
+    
+    app.listen(8000),() => {
+        console.log('listening to port 8000');
+    }
+
+}).catch(error => console.log(error));
+
