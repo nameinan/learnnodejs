ec6042dad2c1c74ed5e9879da6e02062c9987425
Nandakumar Nameirakpam <nandakumar.nameirakpam@tietoevry.com>
2021-12-06 17:25:51 +0530

    add crud operations for the user

---
+++ src/controller/user.controller.ts
@@ -0,0 +1,72 @@
+import { Request,Response } from "express";
+import { getManager } from "typeorm";
+import { User } from "../entity/user.entity";
+import bcryptjs from 'bcryptjs';
+
+
+export const Users = async (req:Request,res:Response) => {
+ 
+    const repository = getManager().getRepository(User);
+    const users = repository.find();
+
+    res.send( (await users).map(u => {
+         const { password,...data}=u;
+         return data;
+    }));
+    
+}
+
+
+
+export const CreateUser = async (req:Request,res:Response) => {
+
+    const{role_id,...body}=req.body;
+    const hashedPassword = await bcryptjs.hash('1234',10);
+    const repository = getManager().getRepository(User);
+    const{password,...user}= await repository.save({
+        ...body,
+        password:hashedPassword
+    });
+    res.send(user);
+
+}
+
+
+
+export const GetUser = async (req:Request,res:Response) => {
+ 
+    const repository = getManager().getRepository(User);
+    const user = await repository.findOne(req.params.id);
+    const userid= req.params.id;
+    if(!user){
+        res.send({
+            message: `User id : ${ userid } not exit `
+        });
+    }
+    const {password,...data}=user;
+    res.send(data);
+   
+}
+
+
+
+
+export const UpdateUser = async (req:Request,res:Response) => {
+
+       const{role_id,...body}=req.body;
+       const repository = getManager().getRepository(User);
+       await repository.update(req.params.id,body);
+       const{ password,...user} = await repository.findOne(req.params.id);
+       res.send(user);
+  
+}
+
+
+export const DeleteUser = async (req:Request,res:Response) => {
+
+    const{role_id,...body}=req.body;
+    const repository = getManager().getRepository(User);
+    await repository.delete(req.params.id);
+    res.status(204).send(null);
+
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,27 @@
 import { Router } from "express";
 import { AuthenticatedUser, Login, Logout, Register, UpdateInfo, UpdatePassword } from "./controller/auth.controller";
 import { News } from "./controller/news.controller";
+import { CreateUser, DeleteUser, GetUser, UpdateUser, Users } from "./controller/user.controller";
 import { AuthMiddleware } from "./middleware/auth.middleware";
 
 export const routes =(router:Router)=>{
+
+    
     router.post('/api/register',Register);
     router.post('/api/login',Login);
     router.get('/api/user', AuthMiddleware,AuthenticatedUser);
     router.post('/api/logout',AuthMiddleware,Logout);
     router.put('/api/user/info',AuthMiddleware,UpdateInfo);
     router.put('/api/user/password',AuthMiddleware,UpdatePassword);
+
+
+    router.get('/api/users', AuthMiddleware, Users);
+    router.post('/api/users', AuthMiddleware, CreateUser);
+    router.get('/api/users/:id', AuthMiddleware, GetUser);
+    router.put('/api/users/:id', AuthMiddleware, UpdateUser);
+    router.delete('/api/users/:id', AuthMiddleware, DeleteUser);
+
+
     router.get('/api/news',News);
 }
 
